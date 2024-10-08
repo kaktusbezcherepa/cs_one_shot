fb2c5f9b6bf7b4428343ee539fc8800644e11a7d
kaktusbezcherepa <eli1starboom@gmail.com>
2024-10-08 15:14:51 +0300

    123

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,21 @@
 import styles from './Header.module.css'
+import Icons from '../../icons/test.svg'
+
 
 const Header = () => {
   return (
     <>
         <header className={styles.header}>
             <div className={styles.logoContainer}>
-                <img src="" alt="" className={styles.logo} />
+                <img src={Icons} alt="" className={styles.logo} />
             </div>
             <nav className={styles.navBar}>
                 <ul className={styles.navList}>
-                    <li className={styles.navLink}>1</li>
-                    <li className={styles.navLink}>2</li>
-                    <li className={styles.navLink}>3</li>
-                    <li className={styles.navLink}>4</li>
-                    <li className={styles.navLink}>5</li>
+                    <li className={styles.navLink}>Сервера</li>
+                    <li className={styles.navLink}>Форум</li>
+                    <li className={styles.navLink}>Поддержка</li>
+                    <li className={styles.navLink}>Информация</li>
+                    
                 </ul>
             </nav>
         </header>
